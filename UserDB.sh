54c07d4ee662cdf3c064ddcc45ccfdb23f0a9e7a
Juan Manuel Oubina <juan.manuel.oubina@litebyte.us>
2017-03-06 13:21:28 -0300

    Modify database script to drop database and create a new one with a new username and password

--- UserDB.sh
+++ UserDB.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-  
-EXPECTED_ARGS=3
+
+EXPECTED_ARGS=2
 E_BADARGS=65
 MYSQL=`which mysql`
 
-Q2="FLUSH PRIVILEGES;"  
-Q3="CREATE DATABASE IF NOT EXISTS $1;"
-Q4="CREATE USER '$2'@'localhost' IDENTIFIED BY '$3';"
-Q5="GRANT USAGE ON *.* TO '$2'@'localhost' IDENTIFIED BY '$3';"
-Q6="GRANT ALL PRIVILEGES ON $1.* TO '$2'@'localhost';"
+Q1="DROP DATABASE IF EXISTS cmz"
+Q2="FLUSH PRIVILEGES;"
+Q3="CREATE DATABASE IF NOT EXISTS cmz;"
+Q4="CREATE USER '$1'@'localhost' IDENTIFIED BY '$2';"
+Q5="GRANT USAGE ON *.* TO '$1'@'localhost' IDENTIFIED BY '$2';"
+Q6="GRANT ALL PRIVILEGES ON cmz.* TO '$1'@'localhost';"
 Q7="FLUSH PRIVILEGES;"
-SQL="${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}"
-  
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}"
+
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 dbname dbuser dbpass"
+  echo "Usage: $0 dbuser dbpass"
   exit $E_BADARGS
 fi
-  
+
 $MYSQL -uroot -p -e "$SQL"
