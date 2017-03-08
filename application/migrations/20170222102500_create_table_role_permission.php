b611aeec5b77a0ae9256eca3e38ecf8e3f11f8d4
Edgar <edgargmc@gmail.com>
2017-03-08 09:19:40 -0300

    add template to recover password

--- application/migrations/20170222102500_create_table_role_permission.php
+++ application/migrations/20170222102500_create_table_role_permission.php
@@ -41,7 +41,7 @@ class Migration_Create_table_role_permission extends CI_Migration{
                 'role_id' => "2"
             ),
             array(
-                'permission_id' => "4",
+                'permission_id' => "2",
                 'role_id' => "2"
             )
         );
