8ac65e980e17aa9903264e55f871000859445df3
Gonza <gonzalo.medinarosa@litebyte.us>
2017-03-09 15:30:55 -0300

    Document scripts

--- push.sh
+++ push.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-pwd
 cd "staging"
+
+# Pull latest changes from the staging branch
 git pull origin staging
 
+#echo "start"
+#until $(curl --silent --fail curl 208.68.39.205/staging/migrate/down); do
+#    printf '.'
+#    sleep 5
+#done
+#echo "done"
+
+# Create push.txt and store last commit pulled data and current date
 today=`date '+%Y-%m-%dT%H:%M:%S'`
-commit=$(git show --summary --pretty=format:"%h%x09%an%x09%ad%x09%s")
-message="$today $commit"
-echo ${message} > push.txt
+echo Last push: ${today} > push.txt
+TZ=UTC git --no-pager log --pretty=format:"%h%x09%an%x09%ai%x09%s" -n5 >> push.txt
