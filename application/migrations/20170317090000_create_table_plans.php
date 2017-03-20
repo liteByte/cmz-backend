0cc9d69fa6c157d708d3fb69e9206071fa1a3088
Juan Manuel Oubina <juan.manuel.oubina@litebyte.us>
2017-03-20 12:44:38 -0300

    Fix plans table to have a composite unique index on medical insurance id and plan description

--- application/migrations/20170317090000_create_table_plans.php
+++ application/migrations/20170317090000_create_table_plans.php
@@ -47,6 +47,7 @@ class Migration_Create_table_plans extends CI_Migration{
         ));
         $this->dbforge->add_field('CONSTRAINT FOREIGN KEY (medical_insurance_id) REFERENCES medical_insurance(medical_insurance_id)');
         $this->dbforge->add_key('plan_id', TRUE);
+        $this->dbforge->add_field('UNIQUE KEY plan_key (medical_insurance_id,description)');
         $this->dbforge->create_table('plans');
 
         $data = array(
