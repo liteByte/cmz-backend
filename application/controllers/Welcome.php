54645f706afa4bedd6a8805f68502613d9858825
Edgar <edgargmc@gmail.com>
2017-04-25 09:13:58 -0300

    test create pdf

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,39 +3,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
 
 
-	function __construct(){
-		parent::__construct();
+    function __construct(){
+        parent::__construct();
+        $this->load->library('Pdf');
+        $this->load->library('session');
+    }
+    public function index(){
 
-		$this->load->library('session');
-	}
-	public function index()
-	{
+        //$this->load->view('welcome_message');
 
-
-		$_SESSION['usuario'] = 'CMZ';
-
-		//$key = 'usuario';
-		 //echo $this->session->userdata($key);
-		//exit;
-		$this->load->view('welcome_message');
-
-
-	}
+        $pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+        $pdf->SetTitle('Factura');
+        $pdf->SetHeaderMargin(30);
+        $pdf->SetTopMargin(20);
+        $pdf->setFooterMargin(20);
+        $pdf->SetAutoPageBreak(true);
+        $pdf->SetAuthor('Author');
+        $pdf->SetDisplayMode('real', 'default');
+        $pdf->AddPage();
+        $pdf->Write(5, 'Some sample text');
+        $pdf->Output('My-File-Name.pdf', 'I');
+        
+    }
 }
---
+++ application/views/pdfreport.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+$pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+$pdf->SetTitle('Factura');
+$pdf->SetHeaderMargin(30);
+$pdf->SetTopMargin(20);
+$pdf->setFooterMargin(20);
+$pdf->SetAutoPageBreak(true);
+$pdf->SetAuthor('Author');
+$pdf->SetDisplayMode('real', 'default');
+
+$pdf->AddPage();
+
+$pdf->Write(5, 'Some sample text');
+$pdf->Output('My-File-Name.pdf', 'I');
+
+
+
+?>
