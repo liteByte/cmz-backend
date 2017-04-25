a96de08d44c906d3a7b10aa2214bf6697aabcd11
Edgar <edgargmc@gmail.com>
2017-04-25 14:20:49 -0300

    add another library

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -35,20 +35,11 @@ class Welcome extends CI_Controller {
             "name" => "Obra Social"
             
         ];
-        
-        $this->load->view('pdfreport', $data);
+        $this->pdf->load_view('welcome_message');
+        $this->pdf->render();
+        $this->pdf->stream("welcome.pdf");
 
-//        $pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
-//        $pdf->SetTitle('Factura');
-//        $pdf->SetHeaderMargin(30);
-//        $pdf->SetTopMargin(20);
-//        $pdf->setFooterMargin(20);
-//        $pdf->SetAutoPageBreak(true);
-//        $pdf->SetAuthor('Author');
-//        $pdf->SetDisplayMode('real', 'default');
-//        $pdf->AddPage();
-//        $pdf->Write(5, 'Some sample text');
-//        $pdf->Output('My-File-Name.pdf', 'I');
-//
+
+
     }
 }
