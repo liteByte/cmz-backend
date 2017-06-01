db34478ee1b211c608e71419c0fdb98e8e3c08cf
Juan Manuel Oubina <juan.manuel.oubina@litebyte.us>
2017-06-01 12:32:40 -0300

    Modify credit-debit get

--- application/config/routes.php
+++ application/config/routes.php
@@ -209,6 +209,7 @@ $route['creditdebit/all']             = 'CreditDebitController/deleteAll';      
 ////////////////////////Credit-debit notes routes////////////////////////
 $route['creditdebitNote']             = 'CreditDebitNoteController/creditDebitNote';              //Post -> create credit/debit, Get -> get credit-debits
 $route['creditdebitNote/(:num)']      = 'CreditDebitNoteController/creditDebitNote';              //Delete -> Delete note and associated credit/debits
+$route['creditdebitNote/print/(:num)']= 'CreditDebitNoteController/printNote';                    //Get -> Generate PDF of the note
 
 ////////////////////////Receipt Process////////////////////////
 $route['receipt/print/(:num)']        = 'ReceiptController/receiptPrint/id/$1';                   //Get -> Generates receipt in pdf
