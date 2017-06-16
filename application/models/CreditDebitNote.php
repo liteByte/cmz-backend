036e57ba0882a4f627ae4512470a2dcf976effed
Juan Manuel Oubina <juan.manuel.oubina@litebyte.us>
2017-06-16 11:16:57 -0300

    New feature: a new credit-debit note can change the state of the bill and other notes

--- application/models/CreditDebitNote.php
+++ application/models/CreditDebitNote.php
@@ -98,6 +98,9 @@ class CreditDebitNote extends CI_Model{
         $this->db->trans_complete();
         if ($this->db->trans_status() === FALSE) return ['status' => 'error', 'msg' => 'Error inesperado al generar la nota de débito/crédito'];
 
+        //Check if the new note turns de debt of the bill in 0
+        if(!($this->checkBillDebtWithNewNote($id_bill))) return ['status' => 'error', 'msg' => 'Error inesperado en la validación del valor de la nota cargada'];
+
         return ['status' => 'ok', 'msg' => 'Nota de crédito/débito creada satisfactoriamente'];
 
     }
@@ -134,6 +137,75 @@ class CreditDebitNote extends CI_Model{
 
         return $previousNoteNumber;
 
+
+    }
+
+    //Checks if the balance of the notes + amount already payed + bill total is 0. If so, update bill and notes status
+    private function checkBillDebtWithNewNote($billID){
+
+        //Obtain bill data
+        $this->db->select('B.*');
+        $this->db->from('bill B');
+        $this->db->where('B.id_bill',$billID);
+        $query = $this->db->get();
+
+        if (!$query)                 return false;
+        if ($query->num_rows() == 0) return false;
+
+        $bill = $query->row();
+
+
+        //Obtain the total for the credit-debit notes of the bill that haven't been billed
+        $this->db->select('CDN.*');
+        $this->db->from('credit_debit_note CDN');
+        $this->db->where('CDN.id_bill',$billID);
+        $this->db->where('CDN.annulled',0);
+        $this->db->where('CDN.state <>',3);
+        $query = $this->db->get();
+
+        if (!$query) return false;
+
+        $notes      = $query->result_array();
+        $totalNotes = 0;
+
+        foreach($notes as $note){
+            if($note['document_type'] == 'C'){
+                //Credit note -
+                $totalNotes = $totalNotes - $note['total_note'];
+            }else{
+                //Debit note +
+                $totalNotes = $totalNotes + $note['total_note'];
+            }
+        }
+
+        //Calculate the current debt (Bill total - amount payed + total of the notes)
+        $currentDebt = $bill->total - $bill->amount_paid + $totalNotes;
+
+        //Check if this new note leaves the debt of the bill in 0 (because of the decimals, values less than 1 are considered 0)
+        //If so, update the bill and its benefits
+        if ($currentDebt < 1){
+
+            //Start transaction
+            $this->db->trans_start();
+
+                //Update bill state
+                $this->db->where('id_bill', $billID);
+                $this->db->update('bill', ['state_billing' => 3]);
+
+                //Update all benefits of the fee
+                $this->db->where('id_bill', $billID);
+                $this->db->update('benefits', ['state' => 3]);
+
+                //Update every note of the bill
+                $this->db->where('id_bill', $billID);
+                $this->db->update('credit_debit_note', ['state' => 2]);
+
+            //End transaction and check everything is ok
+            $this->db->trans_complete();
+            if ($this->db->trans_status() === FALSE) return false;
+        }
+
+        return true;
 
     }
 
